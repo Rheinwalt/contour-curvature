Fix boundary effects for double gradients

contour-curvature.py
@@ -60,29 +60,9 @@ for li in tqdm(np.unique(cl)):
     j = np.argmin(y[x == x.min()])
     jr = np.arange(len(x))[x == x.min()]
     j = jr[j]
-    xe = np.concat(
-        (
-            [
-                x[-1],
-            ],
-            x,
-            [
-                x[0],
-            ],
-        )
-    )
-    ye = np.concat(
-        (
-            [
-                y[-1],
-            ],
-            y,
-            [
-                y[0],
-            ],
-        )
-    )
-    j += 1
+    xe = np.concat(([x[-2], x[-1]], x, [x[0], x[1]]))
+    ye = np.concat(([y[-2], y[-1]], y, [y[0], y[1]]))
+    j += 2
     det = ((xe[j] - xe[j - 1]) * (ye[j + 1] - ye[j - 1])) - (
         (xe[j + 1] - xe[j - 1]) * (ye[j] - ye[j - 1])
     )
@@ -98,10 +78,10 @@ for li in tqdm(np.unique(cl)):
     dy = np.gradient(ye, pixel_size)
     ddx = np.gradient(dx, pixel_size)
     ddy = np.gradient(dy, pixel_size)
-    dx = dx[1:-1]
-    dy = dy[1:-1]
-    ddx = ddx[1:-1]
-    ddy = ddy[1:-1]
+    dx = dx[2:-2]
+    dy = dy[2:-2]
+    ddx = ddx[2:-2]
+    ddy = ddy[2:-2]
     crv = (dx * ddy - dy * ddx) / np.sqrt((dx * dx + dy * dy) ** 3)
 
     # write into image